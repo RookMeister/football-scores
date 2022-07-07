08fe8f135275566b182f253f582c3bfb2080a335
RookMeister <rookmeister666@gmail.com>
2022-07-07 13:27:33 +1000

    fix start text

--- src/apps/bot/handlers/start-command.handler.ts
+++ src/apps/bot/handlers/start-command.handler.ts
@@ -6,5 +6,5 @@ export const startCommandHandler = async (ctx: Context) => {
   const { size, column, values } = mainMenu;
   const keyboard = inlineKeyboard(values, size, column);
   const { username } = ctx.dbuser;
-  await ctx.reply(`Привет @${username}.\nЭти кнопки помогут тебе в получении информации нужной информации. Так же можно поощрить автора за его работу по кнопке "Donate".\nПо любым вопросам писать @RookMeister`, keyboard);
+  await ctx.reply(`Привет @${username}.\nЭти кнопки помогут тебе в получении нужной информации. Так же можно поощрить автора за его работу по кнопке "Donate".\nПо любым вопросам писать @RookMeister`, keyboard);
 };
