c2b9c014f775cc4fe8a53f93817218640c52c56b
RookMeister <rookmeister666@gmail.com>
2022-08-03 16:55:42 +1000

    add toogle darkTheme

--- src/apps/web-app/src/views/HomePage.vue
+++ src/apps/web-app/src/views/HomePage.vue
@@ -11,10 +11,26 @@
           <ion-title size="large">{{ $route.name }}</ion-title>
         </ion-toolbar>
       </ion-header>
+      <ion-list>
+        <ion-item lines="full">
+          <ion-icon slot="start" :icon="moonOutline" />
+          <ion-label>Тёмная тема</ion-label>
+          <ion-toggle @ionChange="changeToggle" :checked="isDark" slot="end"></ion-toggle>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts" setup>
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { ref } from 'vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonIcon, IonItem, IonLabel, IonList, IonToggle } from '@ionic/vue';
+import { moonOutline } from 'ionicons/icons';
+const isDark = ref(false)
+// const checkToggle = (shouldCheck: boolean) =>  isDark.value = shouldCheck;
+const checkTheme = () => (isDark.value = !!document.body.className.match('dark'));
+const changeToggle = (ev: CustomEvent) => document.body.classList.toggle('dark', ev.detail.checked);
+// const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+checkTheme();
+
 </script>
